e2dd502769efbab5d7f6b32466ca7f635ac1f7f2
Lasom729 <ihb0729@gmail.com>
2025-11-19 21:08:53 +0900

    protected game

---
+++ Assets/Scripts/ObjectToProtect.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ObjectToProtect : Entity
+{
+    private Transform player;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        player = FindFirstObjectByType<Player>().transform;
+    }
+
+    protected override void Update()
+    {
+        HandleFlip();
+    }
+
+    protected override void HandleFlip()
+    {
+        if (player == null)
+            return;
+
+        if (player.transform.position.x > transform.position.x && facingRight == false)
+            Flip();
+        else if (player.transform.position.x < transform.position.x && facingRight == true)
+            Flip();
+    }
+
+    protected override void Die()
+    {
+        UI.instance.EnableGameOverUI();
+        base.Die();
+    }
+}
